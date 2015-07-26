f2db59997e4cc17b5548ad2b7399ba4f09334f2c
Oleg G.Kapranov <lugatex@yahoo.com>
2015-07-26 10:35:48 +0300

    add token

--- server/api/models/User.js
+++ server/api/models/User.js
@@ -1,6 +1,9 @@
+var bcrypt = require('bcrypt');
+
 /**
 * User.js
 *
+* @module      :: Model
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
